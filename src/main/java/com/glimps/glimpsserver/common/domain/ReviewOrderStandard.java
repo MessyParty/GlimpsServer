c3d1a42f570918321b83f0a5da8cb7ed0c5cac32
hoo47 <sihookang47@gmail.com>
2023-05-07 18:35:53 +0900

    Fix: change heartCnt to heartsCnt

--- src/main/java/com/glimps/glimpsserver/common/domain/ReviewOrderStandard.java
+++ src/main/java/com/glimps/glimpsserver/common/domain/ReviewOrderStandard.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ReviewOrderStandard {
-	DATE("createdAt"), HEARTS_COUNT("heartCnt");
+	DATE("createdAt"), HEARTS_COUNT("heartsCnt");
 
 	private final String property;
 
