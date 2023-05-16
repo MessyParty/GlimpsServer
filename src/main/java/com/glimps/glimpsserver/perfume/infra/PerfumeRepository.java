859a0bdd8d36cda144c67c5c61f5f6f9689073a6
hoo47 <sihookang47@gmail.com>
2023-05-17 00:17:49 +0900

    Fix: remove lock

--- src/main/java/com/glimps/glimpsserver/perfume/infra/PerfumeRepository.java
+++ src/main/java/com/glimps/glimpsserver/perfume/infra/PerfumeRepository.java
@@ -1,19 +1,13 @@
 package com.glimps.glimpsserver.perfume.infra;
 
+import com.glimps.glimpsserver.perfume.domain.Perfume;
 import java.util.Optional;
 import java.util.UUID;
-
-import javax.persistence.LockModeType;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
-
-import com.glimps.glimpsserver.perfume.domain.Perfume;
 
 public interface PerfumeRepository extends JpaRepository<Perfume, Long> {
 
-	@Lock(LockModeType.PESSIMISTIC_WRITE)
 	Optional<Perfume> findByUuid(UUID uuid);
 
 	@Query("select p from Perfume p "
