0c005c93432829a3eea4f7a94419999501f1cec6
sihoo kang <gyeong0308@gmail.com>
2023-07-23 21:25:49 +0900

    Fix internal error (#44)

--- src/main/java/com/glimps/glimpsserver/review/application/ReviewHeartService.java
+++ src/main/java/com/glimps/glimpsserver/review/application/ReviewHeartService.java
@@ -1,26 +1,21 @@
 package com.glimps.glimpsserver.review.application;
 
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
 import com.glimps.glimpsserver.common.error.EntityNotFoundException;
 import com.glimps.glimpsserver.common.error.ErrorCode;
 import com.glimps.glimpsserver.review.domain.Review;
 import com.glimps.glimpsserver.review.domain.ReviewHeart;
-import com.glimps.glimpsserver.review.infra.ReviewHeartCustomRepository;
 import com.glimps.glimpsserver.review.infra.ReviewHeartRepository;
 import com.glimps.glimpsserver.user.domain.User;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional(readOnly = true)
 public class ReviewHeartService {
 	private final ReviewHeartRepository reviewHeartRepository;
-	private final ReviewHeartCustomRepository reviewHeartCustomRepository;
 
-	public ReviewHeartService(ReviewHeartRepository reviewHeartRepository,
-		ReviewHeartCustomRepository reviewHeartCustomRepository) {
+	public ReviewHeartService(ReviewHeartRepository reviewHeartRepository) {
 		this.reviewHeartRepository = reviewHeartRepository;
-		this.reviewHeartCustomRepository = reviewHeartCustomRepository;
 	}
 
 	@Transactional
@@ -31,7 +26,7 @@ public class ReviewHeartService {
 
 	@Transactional
 	public ReviewHeart cancelReviewHeart(Review review, User user) {
-		ReviewHeart reviewHeart = reviewHeartCustomRepository.findByReviewAndUser(review.getUuid(), user.getId())
+		ReviewHeart reviewHeart = reviewHeartRepository.findByReviewAndUser(review.getUuid(), user.getId())
 			.orElseThrow(() -> new EntityNotFoundException(ErrorCode.REVIEW_HEART_NOT_FOUND));
 		reviewHeart.cancelReviewHeart();
 		reviewHeartRepository.delete(reviewHeart);
